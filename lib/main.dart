6fd65e36ab03987b014ab743ae28e6223bcba382
JeneeDurai <jenee.durai95@gmail.com>
2020-03-03 15:57:31 +0530

    Image View

--- lib/main.dart
+++ lib/main.dart
@@ -2,27 +2,33 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
 
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('Flutter Demo Application'),
-      centerTitle: true,
-      backgroundColor: Colors.red[700],
-    ),
-    body: Center(
-      child:Text("This is my body",
-          style:TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color:Colors.pink[900],
-            fontFamily: 'IndieFlower',
-          )),
-    ),
-    floatingActionButton: FloatingActionButton(
-      child:Text("click"),
-      backgroundColor: Colors.red[400]
-    ),
-  ),
-
+  home: Home()
 
 ));
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Demo Application'),
+        centerTitle: true,
+        backgroundColor: Colors.red[700],
+      ),
+      body: Center(
+//        child: Image(
+//          //image: NetworkImage('https://images.unsplash.com/photo-1548032885-b5e38734688a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=307&q=80'),
+//          image:AssetImage('assets/bg.jpg')
+//        ),
+          //child:Image.asset('assets/bg.jpg')
+          child:Image.network('https://images.unsplash.com/photo-1548032885-b5e38734688a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=307&q=80')
+
+    ),
+      floatingActionButton: FloatingActionButton(
+          child:Text("click"),
+          backgroundColor: Colors.red[400]
+      ),
+    );
+  }
+}
