771f45b5f74d7364ca305bc7c82deee72c70c6aa
JeneeDurai <jenee.durai95@gmail.com>
2020-03-03 20:20:24 +0530

    Containers

--- lib/main.dart
+++ lib/main.dart
@@ -16,46 +16,12 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[700],
       ),
-      body: Center(
-//        child: Image(
-//          //image: NetworkImage('https://images.unsplash.com/photo-1548032885-b5e38734688a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=307&q=80'),
-//          image:AssetImage('assets/bg.jpg')
-//        ),
-          //child:Image.asset('assets/bg.jpg')
-          //child:Image.network('https://images.unsplash.com/photo-1548032885-b5e38734688a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=307&q=80')
-
-//        child:Icon(
-//          Icons.airport_shuttle,
-//          color:Colors.lightBlue,
-//          size:50.0,
-//        )
-
-//          child:RaisedButton(
-//              onPressed: (){
-//                print('Button clicked');
-//              },
-//            child:Text("Btn"),
-//            color:Colors.lightBlue
-//          )
-
-//          child: RaisedButton.icon(
-//            onPressed: (){},
-//            icon: Icon(
-//              Icons.mail,
-//            ),
-//            label: Text("Mail Me"),
-//            color:Colors.amber
-//          ),
-
-            child:IconButton(
-                icon: Icon(Icons.alternate_email),
-                onPressed: (){
-                  print('Icon clicked');
-                },
-              color:Colors.amber
-
-            )
-    ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20.0, vertical:50.0),
+        margin: EdgeInsets.all(30.0),
+          color:Colors.grey[400],
+        child:Text("Hellooo")
+      ),
       floatingActionButton: FloatingActionButton(
           child:Text("click"),
           backgroundColor: Colors.red[400]
