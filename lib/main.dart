d5aaac42c864939890768669aab1c52f9b1e0f4e
JeneeDurai <jenee.durai95@gmail.com>
2020-03-25 09:03:28 +0530

    expanded widget

--- lib/main.dart
+++ lib/main.dart
@@ -16,27 +16,59 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[700],
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.end,
+      body: Row(
+//        mainAxisAlignment: MainAxisAlignment.end,
+//        crossAxisAlignment: CrossAxisAlignment.end,
         children: <Widget>[
-          Row(
-            children:<Widget>[
-              Text('Jeneepan'),
-              Text('Rajadurai')
-            ]
+
+          Expanded(
+            flex:3,
+              child: Image.asset("assets/bg.jpg")),
+          Expanded(
+            flex: 3,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.cyan,
+              child:Text('1')
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+                padding: EdgeInsets.all(30.0),
+                color: Colors.pinkAccent,
+                child:Text('2')
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+                padding: EdgeInsets.all(30.0),
+                color: Colors.amber,
+                child:Text('3')
+            ),
           ),
 
-          FlatButton(
-            onPressed: (){},
-            color:Colors.amber,
-            child:Text('Click me')
-          ),
-          Container(
-            color:Colors.lightBlue,
-            padding:EdgeInsets.all(20.0),
-            child:Text("I am an container")
-          )
+
+
+
+//          Row(
+//            children:<Widget>[
+//              Text('Jeneepan'),
+//              Text('Rajadurai')
+//            ]
+//          ),
+//
+//          FlatButton(
+//            onPressed: (){},
+//            color:Colors.amber,
+//            child:Text('Click me')
+//          ),
+//          Container(
+//            color:Colors.lightBlue,
+//            padding:EdgeInsets.all(20.0),
+//            child:Text("I am an container")
+//          )
         ],
       ),
       floatingActionButton: FloatingActionButton(
